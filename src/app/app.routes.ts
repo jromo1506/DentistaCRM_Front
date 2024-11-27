afc5e567861141013bf4e885bd72a293f8c6a152
Fer3Orenday <fer.orenday3@gmail.com>
2024-11-27 16:37:53 -0600

    Dropdown con scroll

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { DebugComponent } from './pages/debug/debug.component';
 import { UserLoginComponent } from './pages/users/user-login/user-login.component';
 import { UserRegisterComponent } from './pages/users/user-register/user-register.component';
+import { ListComponent } from './components/list/list.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { TableComponent } from './components/table/table.component';
 
 export const routes: Routes = [
     {path:"debug",component:DebugComponent},
     {path:"login",component:UserLoginComponent},
-    {path:"registro",component:UserRegisterComponent}
+    {path:"registro",component:UserRegisterComponent},
+    {path:"list",component:ListComponent},
+    {path:"dropdown",component:DropdownComponent},
+    {path:"table",component:TableComponent},
+    
 ];
--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent {
+  @Input() options: string[] = [];
+
+  isOpen = false;
+
+  selectedOption: string | null = null;
+
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isOpen = false;
+  }
 
 }
