2e1ad674aa86ba693c8451ff87cc8ea8d53c876e
Fer3Orenday <fer.orenday3@gmail.com>
2024-12-03 15:48:19 -0600

    Logo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  
+  menuActive: boolean = false;
+
+  toggleMenu(): void {
+    this.menuActive = !this.menuActive;
+  }
 }
